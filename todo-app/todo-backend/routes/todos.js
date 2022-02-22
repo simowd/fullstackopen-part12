2372e0a69072553b8dc3c5534b4e952876b62a8a
Martin Campuzano <martin.campuzano.flores@gmail.com>
2022-02-22 17:49:31 -0400

    Finished 12.7

--- todo-app/todo-backend/routes/todos.js
+++ todo-app/todo-backend/routes/todos.js
@@ -35,12 +35,14 @@ singleRouter.delete('/', async (req, res) => {
 
 /* GET todo. */
 singleRouter.get('/', async (req, res) => {
-  res.sendStatus(405); // Implement this
+  const todo = await Todo.findById(req.params.id);
+  res.send(todo);
 });
 
 /* PUT todo. */
 singleRouter.put('/', async (req, res) => {
-  res.sendStatus(405); // Implement this
+  const todo = await Todo.findByIdAndUpdate(req.params.id, req.body);
+  res.send(todo);
 });
 
 router.use('/:id', findByIdMiddleware, singleRouter)
